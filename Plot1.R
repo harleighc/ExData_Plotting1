30e7874413fa403ae5614be9c4a89288b56495e3
harleighc <harleighccampbell@gmail.com>
2015-03-07 18:07:43 +1300

    subsetting change

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,10 @@
-data<-read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
 
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
-data<- subset(data, data$Date == "2007-2-1" | data$Date == "2007-2-2")
+d<-read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+data<-d
+
+data$datetime <- as.POSIXct(paste(data$Date,data$Time), format="%d/%m/%Y %H:%M:%S") 
+
+data <- subset(data, datetime >= '2007-2-1 00:00:00' & datetime <= '2007-2-2 23:59:59')
 
 png("plot1.png", width = 480, height = 480)
 
